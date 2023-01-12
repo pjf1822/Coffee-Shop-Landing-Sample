a723fdd7f64cae360ac491a575c53e345c136eb3
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2023-01-11 23:24:23 -0500

    navbar started

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// Footer Items
+// Arrays
 
 const footerArray = [
   "Contact Us",
@@ -11,12 +11,75 @@ const footerArray = [
   " My account",
 ];
 
-var select = document.querySelector(".footer-list");
+const navArray = ["Online Store", "Cafe Locations", "Menu", "About", "Careers"];
+
+const navItem0 = [
+  "coffee",
+  "gift cards",
+  "apparel",
+  "Mugs & Accessories",
+  "Gift Baskets",
+];
+
+const navItem2 = ["Menu Items", "Nutritional Value"];
+const navItem4 = ["Online Application", "Print Application"];
+
+// creating the footer
+
+var footerWrapper = document.querySelector(".footer-list");
 
 for (var i = 0; i < footerArray.length; i++) {
-  var listItem = footerArray[i];
-  var el = document.createElement("li");
-  el.classList.add("footer-list-item");
-  el.textContent = listItem;
-  select.appendChild(el);
+  var listItemContent = footerArray[i];
+  var footerListItem = document.createElement("li");
+  footerListItem.classList.add("footer-list-item");
+  footerListItem.textContent = listItemContent;
+  footerWrapper.appendChild(footerListItem);
+}
+
+// creating the nav bar
+
+var navList = document.querySelector(".nav_itemsList");
+
+for (var i = 0; i < navArray.length; i++) {
+  var navListItemContent = navArray[i];
+  var navListItem = document.createElement("li");
+  navListItem.classList.add(`nav_listItem${i}`, "nav_listItem");
+  navListItem.textContent = navListItemContent;
+  navList.appendChild(navListItem);
+}
+
+// First Nav Item
+
+var navList0 = document.querySelector(".nav_listItem0");
+
+for (var i = 0; i < navItem0.length; i++) {
+  var navListItem0Content = navItem0[i];
+  var navListItem0 = document.createElement("li");
+  navListItem0.classList.add("nav_listItemDropdown");
+  navListItem0.textContent = navListItem0Content;
+  navList0.appendChild(navListItem0);
+}
+
+// Third Nav Item
+
+var navList2 = document.querySelector(".nav_listItem2");
+
+for (var i = 0; i < navItem2.length; i++) {
+  var navListItem2Content = navItem2[i];
+  var navListItem2 = document.createElement("li");
+  navListItem2.classList.add("nav_listItemDropdown");
+  navListItem2.textContent = navListItem2Content;
+  navList2.appendChild(navListItem2);
+}
+
+// Fifth Nav Item
+
+var navList4 = document.querySelector(".nav_listItem4");
+
+for (var i = 0; i < navItem4.length; i++) {
+  var navListItem4Content = navItem4[i];
+  var navListItem4 = document.createElement("li");
+  navListItem4.classList.add("nav_listItemDropdown");
+  navListItem4.textContent = navListItem4Content;
+  navList4.appendChild(navListItem4);
 }
